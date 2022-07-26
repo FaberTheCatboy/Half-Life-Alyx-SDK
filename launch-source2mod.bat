4ccbcd5d52769b85a59c6191db025bf421fa46d5
Faber <103538724+FaberTheCatboy@users.noreply.github.com>
2022-07-26 08:59:15 -0700

    Update launch-source2mod.bat

--- launch-source2mod.bat
+++ launch-source2mod.bat
@@ -1,1 +1,1 @@
-game\bin\win64\source2mod.exe -game hl2 -sw -tools -toolsvr -novr -vconsole -noasserts -nopassiveasserts -dev -devcomp -nop4
+game\bin\win64\source2mod.exe -sw -tools -toolsvr -novr -vconsole -noasserts -nopassiveasserts -dev -devcomp -nop4
